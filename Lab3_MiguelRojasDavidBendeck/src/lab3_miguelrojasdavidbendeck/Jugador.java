2d4fc23cb2eac7b217bab4e7c69db55c3988ce12
Miguel Rojas <m_rojas18@unitec.edu>
2020-02-07 15:09:46 -0600

    Clase Persona, Clase Jugador

---
+++ Lab3_MiguelRojasDavidBendeck/src/lab3_miguelrojasdavidbendeck/Jugador.java
@@ -0,0 +1,107 @@
+package lab3_miguelrojasdavidbendeck;
+
+public class Jugador  extends Persona{
+    private int numero;
+    private int tiro_3;
+    private int defensa;
+    private int tiro_media;
+    private int rebote;
+    private int bandeja;
+    private int pases;
+    private int posteo;
+    private Double altura;
+
+    
+    public Jugador(int numero, int tiro_3, int defensa, int tiro_media, int rebote, int bandeja, int pases, int posteo, Double altura, String nombre, String apellido, int an_profesional, int salario) {
+        super(nombre, apellido, an_profesional, salario);
+        this.numero = numero;
+        this.tiro_3 = tiro_3;
+        this.defensa = defensa;
+        this.tiro_media = tiro_media;
+        this.rebote = rebote;
+        this.bandeja = bandeja;
+        this.pases = pases;
+        this.posteo = posteo;
+        this.altura = altura;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getTiro_3() {
+        return tiro_3;
+    }
+
+    public void setTiro_3(int tiro_3) {
+        this.tiro_3 = tiro_3;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getTiro_media() {
+        return tiro_media;
+    }
+
+    public void setTiro_media(int tiro_media) {
+        this.tiro_media = tiro_media;
+    }
+
+    public int getRebote() {
+        return rebote;
+    }
+
+    public void setRebote(int rebote) {
+        this.rebote = rebote;
+    }
+
+    public int getBandeja() {
+        return bandeja;
+    }
+
+    public void setBandeja(int bandeja) {
+        this.bandeja = bandeja;
+    }
+
+    public int getPases() {
+        return pases;
+    }
+
+    public void setPases(int pases) {
+        this.pases = pases;
+    }
+
+    public int getPosteo() {
+        return posteo;
+    }
+
+    public void setPosteo(int posteo) {
+        this.posteo = posteo;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "numero=" + numero + ", tiro_3=" + tiro_3 + ", defensa=" + defensa + ", tiro_media=" + tiro_media + ", rebote=" + rebote + ", bandeja=" + bandeja + ", pases=" + pases + ", posteo=" + posteo + ", altura=" + altura + '}';
+    }
+
+    
+    
+}
---
+++ Lab3_MiguelRojasDavidBendeck/src/lab3_miguelrojasdavidbendeck/Persona.java
@@ -0,0 +1,55 @@
+package lab3_miguelrojasdavidbendeck;
+
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    private int an_profesional;
+    private int salario;
+
+    public Persona(String nombre, String apellido, int an_profesional, int salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.an_profesional = an_profesional;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getAn_profesional() {
+        return an_profesional;
+    }
+
+    public void setAn_profesional(int an_profesional) {
+        this.an_profesional = an_profesional;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", an_profesional=" + an_profesional + ", salario=" + salario + '}';
+    }
+    
+    
+}
