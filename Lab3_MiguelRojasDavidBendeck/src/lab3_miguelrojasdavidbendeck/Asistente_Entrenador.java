19ddc747fd263ccf372381c1e6d3eb30b38dea8f
Miguel Rojas <m_rojas18@unitec.edu>
2020-02-07 15:39:04 -0600

    Clase Entrenador y sus SubClases Agregadas

---
+++ Lab3_MiguelRojasDavidBendeck/src/lab3_miguelrojasdavidbendeck/Asistente_Entrenador.java
@@ -0,0 +1,9 @@
+package lab3_miguelrojasdavidbendeck;
+
+public class Asistente_Entrenador extends Entrenador{
+    
+    public Asistente_Entrenador(boolean played, String jugada_fav, int salario, String nombre, String apellido, int an_profesional) {
+        super(played, jugada_fav, salario, nombre, apellido, an_profesional);
+    }
+     
+}
---
+++ Lab3_MiguelRojasDavidBendeck/src/lab3_miguelrojasdavidbendeck/Preparador_Fisico.java
@@ -0,0 +1,10 @@
+package lab3_miguelrojasdavidbendeck;
+
+
+public class Preparador_Fisico extends Entrenador{
+    
+    public Preparador_Fisico(boolean played, String jugada_fav, int salario, String nombre, String apellido, int an_profesional) {
+        super(played, jugada_fav, salario, nombre, apellido, an_profesional);
+    }
+
+}
